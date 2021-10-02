e440375cfbd75285192b68a7557cfce81b473b5d
naptracks <cesar.martel9@gmail.com>
2021-10-02 15:51:01 +0200

    composer install and wp-graphql-acf install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '915164404f816854d1dd67997ea199a6a5e6dc8b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '915164404f816854d1dd67997ea199a6a5e6dc8b',
+            'dev_requirement' => false,
+        ),
+        'wp-graphql/wp-graphql-acf' => array(
+            'pretty_version' => 'v0.5.3',
+            'version' => '0.5.3.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../wp-graphql/wp-graphql-acf',
+            'aliases' => array(),
+            'reference' => '0ac0529697d67192ce6b0e9fc27c5dfb3807250c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
